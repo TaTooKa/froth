a1051309adee87b982b2636952c681bbc2c7bd6d
TaTooKa <leandro.rolando@gmail.com>
2024-11-13 03:07:54 -0300

    basic changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Erzählspiel'
+copyright = '2024, TaTooKa'
+author = 'TaTooKa'
 
 release = '0.1'
 version = '0.1.0'

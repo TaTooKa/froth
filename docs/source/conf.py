f2429b1e3d2437c8406e40212df4ebc67a45ffe2
TaTooKa <leandro.rolando@gmail.com>
2024-11-13 03:16:08 -0300

    add custom folders and new req.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,16 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_logo = "_static/images/Erzahlspiel-logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+}
+
+def setup(app):
+    app.add_css_file('css/custom.css')
